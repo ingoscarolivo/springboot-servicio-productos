8a3c9581766aade0a93c5c6e56a883d6fa02f796
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-02-26 22:10:36 -0500

    configuracion para prueba de servidor zuul API Gateway

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -16,8 +16,8 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 @RestController
 public class ProductoController {
 
-	//@Autowired
-	//private Environment env;
+	@Autowired
+	private Environment env;
 
 	@Value("${server.port}")
 	private Integer port;
@@ -28,8 +28,8 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoService.findAll().stream().map(producto -> {
-			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-			producto.setPort(port);
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -37,8 +37,8 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		producto.setPort(port);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(port);
 		/*try {
 			Thread.sleep(2000L);
 		} catch (InterruptedException e) {
