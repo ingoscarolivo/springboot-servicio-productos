b5b87da6652adcd5f6cb0c2c373baaaf45720244
Oscar Olivo <oscar.olivo@ceiba.com.co>
2023-03-01 22:29:27 -0500

    Creacion de CRUD para producto

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,10 +5,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
@@ -45,6 +45,30 @@ public class ProductoController {
 			e.printStackTrace();
 		}*/
 		return producto;
+	}
+
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto){
+		return productoService.save(producto);
+
+	}
+
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id){
+		Producto productoDB = productoService.findById(id);
+
+		productoDB.setNombre(producto.getNombre());
+		productoDB.setPrecio(producto.getPrecio());
+
+		return productoService.save(productoDB);
+	}
+
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id){
+		productoService.deleteById(id);
 	}
 
 }
